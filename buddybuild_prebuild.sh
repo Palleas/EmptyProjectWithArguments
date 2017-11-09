a206f14d64540629d461b694875c5585d327b34c
Romain Pouclet <romain.pouclet@gmail.com>
2017-11-09 17:50:32 -0500

    Add debug / atually enable spotlight

--- buddybuild_prebuild.sh
+++ buddybuild_prebuild.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 sudo launchctl load -w /System/Library/LaunchDaemons/com.apple.metadata.mds.plist
+sudo mdutil -a -i on
 
 chruby 2.3.1
 
@@ -8,5 +9,12 @@ git clone git@github.com:CocoaPods/cocoadocs.org.git
 cd cocoadocs.org
 gem install bundler
 bundle install
+
+echo "Swift Versions..."
 bundle exec rake swift_versions
+
+echo "."
+echo "."
+
+echo "Generating doc"
 bundle exec ruby cocoadocs.rb cocoadocs url https://raw.githubusercontent.com/CocoaPods/Specs/master/Specs/9/f/b/ReactiveSwift/3.0.0-rc.1/ReactiveSwift.podspec.json
